55d3d9410662b9b4e7c026293ab9567773df7c01
EpiDoubleHaxe <edouard.chhang@epitech.eu>
2022-05-03 10:04:13 +0200

    push

--- src/particle/particle.c
+++ src/particle/particle.c
@@ -19,20 +19,34 @@ int create_particle(game_t *game)
     sfSprite_setTexture(particle->sprite, particle->texture, sfFalse);
     particle->pos = ((sfVector2f){.x = 500, .y = 500});
     sfSprite_setPosition(particle->sprite, particle->pos);
-    add_node(particle, &(game->particles));
+    add_end_node(particle, &(game->particles));
     return 0;
 }
 
 int update_particle(game_t *game)
 {
+    particle_t *particle = game->particles->data;
+    linked_list_t *index = game->particles;
+
+    while (index != NULL) {
+        particle = index->data;
+        particle->pos.x++;
+        sfSprite_setPosition(particle->sprite, particle->pos);
+        index = index->next;
+    }
     return 0;
 }
 
 int draw_particle(game_t *game)
 {
-    particle_t *particle = game->particles->data;
+    particle_t *particle = NULL;
+    linked_list_t *index = game->particles;
 
-    sfRenderWindow_drawSprite(game->window, particle->sprite, NULL);
+    while (index != NULL) {
+        particle = index->data;
+        sfRenderWindow_drawSprite(game->window, particle->sprite, NULL);
+        index = index->next;
+    }
     return 0;
 }
 
